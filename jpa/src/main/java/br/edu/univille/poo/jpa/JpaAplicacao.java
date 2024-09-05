64af35af5cebc106b7c1b7b1ff3decb5e855bd5c
Isabel Baggio <130410760+isahbaggio@users.noreply.github.com>
2024-09-05 14:47:42 -0300

    Add files via upload

---
+++ jpa/src/main/java/br/edu/univille/poo/jpa/JpaAplicacao.java
@@ -0,0 +1,13 @@
+package br.edu.univille.poo.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAplicacao {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAplicacao.class, args);
+	}
+
+}
---
+++ jpa/target/classes/data.sql
@@ -0,0 +1,2 @@
+insert into pessoa(id,nome,cpf) values(1,'Renee Rapp','000.000.000-00');
+insert into pessoa(id,nome,cpf) values(2,'Zendaya','123.456.789-01');
